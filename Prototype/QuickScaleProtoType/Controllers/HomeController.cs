1bbb6d42db5415896ee3fa58490988469e5951c8
Jorge Polanco <Jorge.L.Polanco@outlook.com>
2019-09-12 11:13:33 -0700

    Unzipped prototype source code and organized folders

---
+++ Prototype/QuickScaleProtoType/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickScaleProtoType.Models;
+
+namespace QuickScaleProtoType.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("FrontPage");
+        }
+
+        public IActionResult CreateAccountPage()
+        {
+            return View();
+        }
+        public IActionResult ScalesPage()
+        {
+            return View();
+        }
+        public IActionResult GamePage()
+        {
+
+            return View("GamePage");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prototype/QuickScaleProtoType/Models/Scale.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickScaleProtoType.Models
+{
+        public class Scale
+        {
+            public string Key { get; set; }
+            public string Mode { get; set; }
+            public int FretNumber { get; set; }
+            public int StringNumber { get; set; }
+            public string FirstString { get; set; }
+            public string SecondString { get; set; }
+            public string ThirdString { get; set; }
+            public string FourthString { get; set; }
+            public string FifthString { get; set; }
+            public string SixthString { get; set; }
+            public string SeventhString { get; set; }
+            public string EightString { get; set; }
+            public string NinthString { get; set; }
+            public string TenthString { get; set; }
+
+        }
+    
+}
