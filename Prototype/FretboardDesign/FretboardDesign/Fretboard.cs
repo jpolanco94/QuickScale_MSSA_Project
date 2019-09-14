ae81bc01b77609066b7efac23fb46b029b6531b9
Jorge Polanco <Jorge.L.Polanco@outlook.com>
2019-09-14 16:18:08 -0700

    Added some preliminary C# code for how I want to display scales.

---
+++ Prototype/FretboardDesign/FretboardDesign/Fretboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FretboardDesign
+{
+    class Fretboard
+    {
+        public int stringNumber { get; set;}
+        public int fretNumber { get; set; }
+        public string rootNote { get; set; }
+        public string scaleMode { get; set; }
+
+    }
+}
---
+++ Prototype/FretboardDesign/FretboardDesign/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace FretboardDesign
+{
+    class Program
+    {
+        static void ScaleNotes (string [] rootNote, string scaleMode, string pentatonic)
+        {
+            if (pentatonic == "majorP")
+            {
+                if(scaleMode == "major")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "harmonic minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == " melodic minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+
+                }
+                if (scaleMode == "aeolian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "mixolydian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "dorian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "lydian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "phrygian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "ionian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "locrian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "blues")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+
+                }
+            }
+            if (pentatonic == "")
+            {
+                if (scaleMode == "major")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[2] + " " + rootNote[4] + " " + rootNote[5] + " " + rootNote[7] + " " + rootNote[9] + " " + rootNote[11]);
+                }
+                if (scaleMode == "minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "harmonic minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == " melodic minor")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+
+                }
+                if (scaleMode == "aeolian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "mixolydian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "dorian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "lydian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "phrygian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "ionian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "locrian")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "blues")
+                {
+                    Console.WriteLine(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+
+                }
+            }
+            if (pentatonic == "minorP")
+            {
+                if (scaleMode == "major")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "minor")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "aeolian")
+                { 
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "mixolydian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "dorian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "lydian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "phrygian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "ionian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "locrian")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+                if (scaleMode == "blues")
+                {
+                    Console.Write(rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0] + " " + rootNote[0]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] a = { "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab" };
+            string[] aSbF = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] b = { "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb" };
+            string[] c = { "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" };
+            string[] cSdF = { "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C" };
+            string[] d = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] dSeF = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] e = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] f = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] fSgF = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] g = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            string[] gSaF = { "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A" };
+            ScaleNotes(a, "major", "majorP");
+        }
+    }
+}
